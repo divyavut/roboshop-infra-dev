5896d187150efba9ea1ef875c1a5c8b8736c35eb
divyavut <vdivyaaws@gmail.com>
2024-12-06 16:21:59 -0500

    roboshop infra dev

--- bash_script_D.sh
+++ bash_script_D.sh
@@ -2,6 +2,6 @@
 for i in 80-cdn 70-ecr 60-alb 50-acm 40-eks 30-rds 20-bastion 10-sg 00-vpc
 do cd $i 
   terraform destroy --auto-approve 
-  echo "$i, has been created" 
+  echo "$i, has been destroyed" 
   cd ..
 done
